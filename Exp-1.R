4f203de0e86abf35355a2ea83407a1147a21c526
Mehak Lal <immehaklal@gmail.com>
2026-09-12 21:40:26 +0530

    Add files via upload

---
+++ Exp-1.R
@@ -0,0 +1,46 @@
+#SAMPLE PROGRAM 
+print("Hello, this is Mehak Lal :-)")
+
+
+
+#ADDITION
+#Example-1 
+x <- 1+2
+x 
+
+
+#Example-2
+26+38
+
+
+
+#SUBTRACTION
+#Example-1
+y <- 2-1
+y
+
+
+#Example-2
+38-26 
+
+
+
+#MULTIPLICATION 
+#Example-1 
+z <- 26*38 
+z
+
+
+#Example-2
+26*3
+
+
+
+#DIVISION 
+#Example-1
+26622/3
+
+
+#Example-2
+266222%%3        #Gives remainder 
+
---
+++ Exp-2.R
@@ -0,0 +1,57 @@
+###################   ASSIGNMENT-2(A)  #################
+
+#VECTORS OF EQUAL LENTGHS 
+Marks1 <- c(10,20,30,40,50)
+Marks2 <- c(60,70,80,90,100) 
+length(Marks1)
+length(Marks2)
+
+
+#ADDITION OF TWO EQUAL VECTORS 
+Marks1 + Marks2 
+
+
+#SUBTRACTION OF TWO EQUAL VECTORS 
+Marks2 - Marks1 
+
+
+#MULTIPLICATION OF TWO EQUAL VECTORS 
+Marks2  *  Marks1 
+
+
+
+#DIVISION OF TWO EQUAL VECTORS 
+Marks2  /  Marks1 
+
+
+
+
+
+
+###################   ASSIGNMENT-2(B)  #################
+
+#VECTORS OF UNEQUAL LENGTHS 
+Marks1 <- c(10,20,30,40,50,60)
+Marks2 <- c(70,80,90,100) 
+length(Marks1)
+length(Marks2)
+
+
+#ADDITION OF TWO UNEQUAL VECTORS 
+Marks1 + Marks2 
+
+
+#SUBTRACTION OF TWO UNEQUAL VECTORS 
+Marks2 - Marks1 
+
+
+#MULTIPLICATION OF TWO UNEQUAL VECTORS 
+Marks2  *  Marks1 
+
+
+
+#DIVISION OF TWO UNEQUAL VECTORS 
+Marks2  /  Marks1 
+
+
+
